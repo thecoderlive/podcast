88f2c2766427b2825573467851b38d526a3853ca
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-19 12:04:05 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,7 @@
+export const item = {
+    "id": "538bc9ee-3f61-4a84-a165-ab99fa50e7e0",
+    "podcast_title": "Significant painting board whose. Early one TV health can edge. Among even production recently morning husband wonder she.",
+    "about_podcast": "Young since behind pick tree black movement. Movie leader future all item every difference daughter. Baby pattern travel perhaps little.",
+    "Podcast_name": "one",
+    "suggestion": "Market three hour ready wonder car any. However discussion eye successful."
+}
